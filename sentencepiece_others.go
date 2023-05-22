877a85e6cd17f2fee4ddf0720028005ff65c5b39
hanleyzhao <hanleyzhao@tencent.com>
2023-05-22 12:04:14 +0800

    add platform supported

--- sentencepiece_others.go
+++ sentencepiece_others.go
@@ -12,8 +12,6 @@ package sentencepiecego
 import "C"
 import (
 	"errors"
-	"fmt"
-	"unsafe"
 )
 
 type SentencePieceProcessor struct {
@@ -22,51 +20,15 @@ type SentencePieceProcessor struct {
 }
 
 func Load(path string) (*SentencePieceProcessor, error) {
-	var sp SentencePieceProcessor
-	cPath := C.CString(path)
-	sp.index = C.loadSentencePieceProcessor(cPath)
-	if sp.index == nil {
-		return &sp, fmt.Errorf("Can not load model: %s\n", path)
-	}
-	sp.beginMaxTokens = 128
-	C.free(unsafe.Pointer(cPath))
-	return &sp, nil
+	return nil, errors.New("platform no supported")
 }
 
 func (sp *SentencePieceProcessor) Encode(text string) ([]int, error) {
-	var size int
-	var ids []int
-	size, ids = sp.encode(text, sp.beginMaxTokens)
-	if size < 0 {
-		return ids, errors.New("Encode error!")
-	}
-	// If size exceed beginMaxTokens, we extend ids size again!
-	if size > sp.beginMaxTokens {
-		size, ids = sp.encode(text, size)
-		if size < 0 {
-			return ids, errors.New("Encode error!")
-		}
-	}
-	return ids, nil
+	return nil, errors.New("platform no supported")
 }
 
 func (sp *SentencePieceProcessor) encode(text string, maxTokens int) (int, []int) {
-	ids := make([]int, 0, maxTokens)
-	CIDs := make([]C.int, maxTokens, maxTokens)
-	cText := C.CString(text)
-	size := int(C.Encode(sp.index, cText, &CIDs[0], C.int(maxTokens)))
-	C.free(unsafe.Pointer(cText))
-	// Size < 0 means error
-	if size < 0 {
-		return size, ids
-	}
-	// Copy value from CIDs to ids if -1 < size < maxTokens
-	if size <= maxTokens {
-		for i := 0; i < size; i++ {
-			ids = append(ids, int(CIDs[i]))
-		}
-	}
-	return size, ids
+	return 0, nil
 }
 
 func (sp *SentencePieceProcessor) Free() {
